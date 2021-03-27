f68b4409cb1b8189713484d463338de11dd9f2bb
m <gorczix@gmail.com>
2021-03-27 10:56:48 +0100

    clean up

--- main/main.ino
+++ main/main.ino
@@ -7,13 +7,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-//#ifdef U8X8_HAVE_HW_SPI
-//#include <SPI.h>
-//#endif
-//#ifdef U8X8_HAVE_HW_I2C
-//#include <Wire.h>
-//#endif
-
 #define ONE_WIRE_BUS 10
 
 OneWire oneWire(ONE_WIRE_BUS);
