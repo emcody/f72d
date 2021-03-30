e9b5779edc91020247db03c7e4459fff9b776e9b
m <gorczix@gmail.com>
2021-03-30 10:19:12 +0200

    print temperature for both front and back sensors

--- main/main.ino
+++ main/main.ino
@@ -12,6 +12,8 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
+DeviceAddress frontThermometer = {0x28, 0x29, 0x64, 0x1B, 0x06, 0x0, 0x0, 0x4D};
+DeviceAddress outsideThermometer = {0x28, 0x85, 0x36, 0x1C, 0x06, 0x00, 0x00, 0xFC};
 
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
@@ -19,10 +21,9 @@ const uint8_t *font = u8g2_font_crox3cb_tf;
 byte digitWidth = 13;
 byte digitHeight = 20;
 
-float tempC;
-float tempC_tmp;
-int num = 123;
-byte xOffset = 0;
+float tempC_front;
+float tempC_back;
+float voltage;
 
 
 void setup(void) {
@@ -32,8 +33,8 @@ void setup(void) {
 
 void loop(void) {
   sensors.requestTemperatures(); // Send the command to get temperatures
-  tempC = sensors.getTempCByIndex(0);
-  printOLED_tmp();
+  tempC_front = sensors.getTempC(frontThermometer);
+  tempC_back = sensors.getTempC(outsideThermometer);
   printOLED();
 }
 
@@ -53,20 +54,22 @@ byte calculateXOffset(float temp) {
 void printOLED(void) {
   u8g2.firstPage();
   do {
+    byte temp1XOffset = calculateXOffset(tempC_front);
     u8g2.setFont(font);
-    u8g2.setCursor(0, 20);
-    u8g2.print(tempC, 1);
-    u8g2.drawGlyph(53, 20, 0x00b0); // degree
-    u8g2.drawStr(63, 20, "C");
+    u8g2.setCursor(0 + temp1XOffset, 20);
+    u8g2.print(tempC_front, 1);
+    u8g2.drawGlyph(73, 20, 0x00b0); // degree
+    u8g2.drawStr(83, 20, "C");
 
-    u8g2.setCursor(20, 40);
-    u8g2.print(tempC, 1);
+    byte temp2XOffset = calculateXOffset(tempC_back);
+    u8g2.setCursor(0 + temp2XOffset, 40);
+    u8g2.print(tempC_back, 1);
     u8g2.drawGlyph(73, 40, 0x00b0); // degree
     u8g2.drawStr(83, 40, "C");
 
     u8g2.setCursor(0, 60);
-    u8g2.print(tempC, 1);
+    u8g2.print(voltage, 1);
     u8g2.drawStr(53, 60, "V");
   } while ( u8g2.nextPage() );
-  delay(1000);
+  delay(2000);
 }
